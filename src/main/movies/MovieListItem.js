2df6453f600c089772aabfeb329a49d8a293b588
ibrahimalausa <ibrahim.alausa@andela.com>
2018-12-10 22:27:07 +0100

    movies list now displaying on frontend

--- src/main/movies/MovieListItem.js
+++ src/main/movies/MovieListItem.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import './MovieListItem.css';
 
-const MovieListItem = ({title}) => <li className='movie-item'>{title}</li>
+const MovieListItem = ({movie}) => {
+   const {title,poster_path} = movie;
+   const imgUrl = `https://image.tmdb.org/t/p/w342/${poster_path}`;
+return(
+   <li className='movie'>
+      <img src={imgUrl} alt="" />
+      <span>{title}</span>
+   </li>
+)
+}
 
 
 export default MovieListItem;
