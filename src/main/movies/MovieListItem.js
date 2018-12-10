91549b7241c94a2131f7d8c755866448d07e61a6
ibrahimalausa <ibrahim.alausa@andela.com>
2018-12-10 22:54:34 +0100

    Movie list displaying with pictures

--- src/main/movies/MovieListItem.js
+++ src/main/movies/MovieListItem.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './MovieListItem.css';
 
 const MovieListItem = ({movie}) => {
-   const {title,poster_path} = movie;
+   const { title, poster_path, release_date, vote_average } = movie;
    const imgUrl = `https://image.tmdb.org/t/p/w342/${poster_path}`;
-return(
-   <li className='movie'>
-      <img src={imgUrl} alt="" />
-      <span>{title}</span>
-   </li>
-)
+   const year = release_date.substring(0, 4);
+
+   return (
+      <li className="movie-item">
+         <img src={imgUrl} alt={title} />
+         <div className="movie-description">
+            <h2>{title}</h2>
+            <section className="movie-details">
+               <div className="movie-year">
+                  <span className="title">Year</span>
+                  <span>{year}</span>
+               </div>
+               <div className="movie-rating">
+                  <span className="title">Rating</span>
+                  <span>{vote_average}</span>
+               </div>
+            </section>
+         </div>
+      </li>
+   );
 }
 
 
